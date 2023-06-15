1bc3ad9638b143197dd40dbe0614d1764fa56d95
Adriana Sanchez Gomez <ad897964@dal.ca>
2023-06-15 07:43:47 -0300

    Added custom job alerts component in landing page

---
+++ frontend/src/Pages/LandingPage/CustomAlertsComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "../../styles/CustomAlertsComponent.css";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBell} from "@fortawesome/free-solid-svg-icons";
+
+function CustomAlertsComponent() {
+
+    return (
+        <div className="customAlertsSection">
+            <div className="customAlerts-background">
+                <div className="customAlerts-content">
+                    <div className="customAlerts-text">
+                        <h2>Get custom job alerts</h2>
+                        <p>
+                            Dal Linked allows users to receive tailored job alerts that align with their specific
+                            preferences and desired job sectors.
+                            By understanding the individual needs of each user, Dal Linked ensures that the job
+                            notifications delivered are highly relevant and personalized.
+                            Whether it's technology, finance, marketing, or any other sector, users can choose
+                            their preferred job sectors, ensuring they receive updates on job opportunities that
+                            best suit their skills and interests.
+                        </p>
+                    </div>
+                    <FontAwesomeIcon icon={faBell} beat style={{color: "#EAE3D2",}} size="2x"/>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default CustomAlertsComponent;
--- frontend/src/Pages/LandingPage/LandingPage.js
+++ frontend/src/Pages/LandingPage/LandingPage.js
@@ -2,11 +2,13 @@ import React from "react";
 import HeroComponent from "./HeroComponent.js";
 import SuccessStoryComponent from "./SuccessStoryComponent.js";
 import TopCompaniesComponent from "./TopCompaniesComponent";
+import CustomAlertsComponent from "./CustomAlertsComponent";
 function LandingPage() {
   return (
     <div className="landingPage">
         <HeroComponent />
         <TopCompaniesComponent />
+        <CustomAlertsComponent />
         <SuccessStoryComponent />
     </div>
   );
