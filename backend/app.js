de1d42a151c60a081bc9954bf20a4250d6b384de
Piyush Akoliya <Piyush.Akoliya@dal.ca>
2023-07-24 16:50:07 -0300

    REsolved merege conflicts and bugs

--- backend/app.js
+++ backend/app.js
@@ -3,21 +3,23 @@ var logger = require("morgan");
 
 const cors = require("cors");
 var app = express();
-var express = require("express");
-var logger = require("morgan");
 require("dotenv").config();
 
-var contactUsRouter = require("./api/routes/contactUs");
 const jobsRouter = require("./api/routes/jobs");
 const announcementsRouter = require("./api/routes/announcements");
 const jobSectorsRouter = require("./api/routes/jobSectors");
 const successStoryRouter = require("./api/routes/successStoryRoute");
+const studentRegRouter = require("./api/routes/studentReg");
+const contactUsRouter = require("./api/routes/contactUs");
+const adminRegRouter = require("./api/routes/adminReg");
+const employerRegRouter = require("./api/routes/employerReg");
+const userRoutes = require("./api/routes/userAuth");
+
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 app.use(cors());
 
-app.use("/contactUs", contactUsRouter);
 app.use("/jobs", jobsRouter);
 app.use("/successStory", successStoryRouter);
 app.use("/contactUs", contactUsRouter);
