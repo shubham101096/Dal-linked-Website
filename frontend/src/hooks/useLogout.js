676db7929a610c38e7fd88904378659013bee673
Piyush Akoliya <Piyush.Akoliya@dal.ca>
2023-07-26 18:01:47 -0300

    Resolved the bug

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,12 @@
 import { useAuthContext } from "./useAuthContext";
-import { useNavigate } from "react-router-dom";
 export const useLogout = () => {
   const { dispatch } = useAuthContext();
-  const navigate = useNavigate();
   const logout = () => {
     // remove user from storage
     localStorage.removeItem("user");
 
     // dispatch logout action
     dispatch({ type: "LOGOUT" });
-    navigate("/");
   };
 
   return { logout };
